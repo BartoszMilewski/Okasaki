4ab96951644fb0ee6689ef76befc6885c2acac1f
Bartosz Milewski <bartosz@relisoft.com>
2013-11-05 18:37:21 -0800

    Lazy Queue

---
+++ LazyQueue/Queue.h
@@ -0,0 +1,38 @@
+#include "LazyStream.h"
+
+template<class T>
+class Queue
+{
+public:
+	Queue() : _lenF(0), _lenR(0) {}
+	Queue(int lf, Stream<T> f, int lr, Stream<T> r)
+		: _lenF(lf), _front(f), _lenR(lr), _rear(r)
+	{}
+	bool isEmpty() const { return _lenF == 0; }
+	Queue push_back(T x) const
+	{
+		return check(_lenF, _front, _lenR + 1, Stream<T>(CellFun<T>(x, _rear)));
+	}
+	T front() const { return _front.get(); }
+	
+	Queue pop_front() const
+	{
+		return check(_lenF - 1, _front.tail(), _lenR, _rear);
+	}
+	// for debugging only
+	int lenF() const { return _lenF; }
+	int lenR() const { return _lenR; }
+	Stream<T> const & fStream() const { return _front; }
+	Stream<T> const & rStream() const { return _rear; }
+private:
+	static Queue check(int lf, Stream<T> f, int lr, Stream<T> r)
+	{
+		if (lr <= lf) return Queue(lf, f, lr, r);
+		return Queue(lf + lr, concat(f, r.reverse()), 0, Stream<T>());
+	}
+private:
+	int _lenF;
+	Stream<T> _front;
+	int _lenR;
+	Stream<T> _rear;
+};
---
+++ LazyQueue/Test.cpp
@@ -0,0 +1,48 @@
+#include "Queue.h"
+#include <iostream>
+
+template<class T>
+void lazyPrint(Stream<T> const & s)
+{
+	if (s.isEmpty())
+	{
+		std::cout << "[]" << std::endl;
+	}
+	else if (s.isForced())
+	{
+		T v = s.get();
+		auto tail = s.tail();
+		std::cout << v << " ";
+		lazyPrint(tail);
+	}
+	else
+	{
+		std::cout << "[?]\n";
+	}
+}
+
+template<class T>
+void printQ(Queue<T> const & q)
+{
+	std::cout << q.lenF() << ", ";
+	lazyPrint(q.fStream());
+	std::cout << q.lenR() << ", ";
+	lazyPrint(q.rStream());
+}
+
+void main()
+{
+	Queue<int> q0;
+	auto q1 = q0.push_back(10);
+	printQ(q1);
+	auto q2 = q1.push_back(20);
+	printQ(q2);
+	auto q3 = q2.push_back(30);
+	printQ(q3);
+	auto x = q3.front();
+	std::cout << "Pop: " << x << std::endl;
+	printQ(q3);
+	std::cout << "Tail\n";
+	auto t1 = q3.pop_front();
+	printQ(t1);
+}
