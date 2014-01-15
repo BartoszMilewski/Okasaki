9be0317714b1c214cb16563602c403547139280e
Bartosz Milewski <bartosz@relisoft.com>
2014-01-15 15:23:32 -0800

    Simplified RB Tree

--- RBTree/RBTree.h
+++ RBTree/RBTree.h
@@ -114,37 +114,50 @@ private:
             return RBTree(R, RBTree(), x, RBTree());
         T y = root();
         Color c = rootColor();
-        if (x < y)
-            return balance(c, left().ins(x), y, right());
-        else if (y < x)
-            return balance(c, left(), y, right().ins(x));
+        if (rootColor() == B)
+        {
+            if (x < y)
+                return balance(left().ins(x), y, right());
+            else if (y < x)
+                return balance(left(), y, right().ins(x));
+            else
+                return *this; // no duplicates
+        }
         else
-            return *this; // no duplicates
+        {
+            if (x < y)
+                return RBTree(c, left().ins(x), y, right());
+            else if (y < x)
+                return RBTree(c, left(), y, right().ins(x));
+            else
+                return *this; // no duplicates
+        }
     }
-    static RBTree balance(Color c, RBTree const & lft, T x, RBTree const & rgt)
+    // Called only when parent is black
+    static RBTree balance(RBTree const & lft, T x, RBTree const & rgt)
     {
-        if (c == B && lft.doubledLeft())
+        if (lft.doubledLeft())
             return RBTree(R
                         , lft.left().paint(B)
                         , lft.root()
                         , RBTree(B, lft.right(), x, rgt));
-        else if (c == B && lft.doubledRight())
+        else if (lft.doubledRight())
             return RBTree(R
                         , RBTree(B, lft.left(), lft.root(), lft.right().left())
                         , lft.right().root()
                         , RBTree(B, lft.right().right(), x, rgt));
-        else if (c == B && rgt.doubledLeft())
+        else if (rgt.doubledLeft())
             return RBTree(R
                         , RBTree(B, lft, x, rgt.left().left())
                         , rgt.left().root()
                         , RBTree(B, rgt.left().right(), rgt.root(), rgt.right()));
-        else if (c == B && rgt.doubledRight())
+        else if (rgt.doubledRight())
             return RBTree(R
                         , RBTree(B, lft, x, rgt.left())
                         , rgt.root()
                         , rgt.right().paint(B));
         else
-            return RBTree(c, lft, x, rgt);
+            return RBTree(B, lft, x, rgt);
     }
     bool doubledLeft() const 
     {
